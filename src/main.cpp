8a1baf8ccf64677a5028d1cb9d0011afd7638119
Cobrand <cobrandw@gmail.com>
2016-03-04 00:25:45 +0100

    unique ptr with auto deleters for sdl/mpv

--- src/main.cpp
+++ src/main.cpp
@@ -7,84 +7,79 @@
 #include <functional>
 
 #include <docopt.h>
-
+/*
 #include "mpv_wrapper.hpp"
 #include "sdl_wrapper.hpp"
-
+*/
 #include <SDL2/SDL.h>
 
 #include <mpv/client.h>
 #include <mpv/opengl_cb.h>
-
+#include "pointers.hpp"
 
 static Uint32 wakeup_on_mpv_redraw, wakeup_on_mpv_events;
-/*
-void on_mpv_events( void * )
-{
-    SDL_Event event ;
-    event.type = wakeup_on_mpv_events;
-    SDL_PushEvent( &event );
-}
 
-void on_mpv_redraw( void * )
-{
-    SDL_Event event ;
-    event.type = wakeup_on_mpv_redraw;
-    SDL_PushEvent( &event );
-}
-*/
 int main( int argc, char *argv[] )
 {
     constexpr char USAGE[]{
         R"(toyunda-player.
      Usage:
-         toyounda-player [options] <file>
+         toyunda-player [options] <file>
 
      Options:
          --invert  invert the screen
     )"};
 
     std::map< std::string, docopt::value > args{docopt::docopt(
-        USAGE, {argv + 1, argv + argc}, true, "toyounda-player" )};
+        USAGE, {argv + 1, argv + argc}, true, "toyunda-player" )};
     auto const mediaFile = args["<file>"].asString();
     const int factor     = args["--invert"].asBool() ? 1 : -1;
 
-    MPV::Handle_ptr mpv{};
+    std::unique_ptr<mpv_handle> mpv(mpv_create());
+    if (!mpv){
+        throw std::runtime_error("mpv context init failed");
+    }
+    if (mpv_initialize(mpv.get()) < 0){
+        throw std::runtime_error("mpv initialization fail");
+    }
 
     if( SDL_Init( SDL_INIT_VIDEO ) < 0 ) {
         throw std::runtime_error("failed to init SDL");
     }
-    SDL::Window_ptr window{SDL_CreateWindow(
+    std::unique_ptr<SDL_Window> window(SDL_CreateWindow(
         "Toyunda Player",
         SDL_WINDOWPOS_CENTERED,
         SDL_WINDOWPOS_CENTERED,
-        1000,
-        500,
-        SDL_WINDOW_OPENGL | SDL_WINDOW_SHOWN | SDL_WINDOW_RESIZABLE )};
+        1280,
+        720,
+        SDL_WINDOW_OPENGL | SDL_WINDOW_SHOWN | SDL_WINDOW_RESIZABLE ));
     if( !window ) {
         throw std::runtime_error("failed to create SDL window");
     }
-
-    MPV::openGL_CB_context mpv_gl{mpv, MPV::sub_api::OPENGL_CB};
-
+    auto ptr = (mpv_opengl_cb_context*) mpv_get_sub_api(mpv.get(), MPV_SUB_API_OPENGL_CB) ;
+    std::unique_ptr<mpv_opengl_cb_context> mpv_gl(ptr);
+    if (!mpv_gl){
+        throw std::runtime_error("failed to create mpv GL API handle");
+    }
+    // retrieve opengl context DO NOT DELETE
     SDL_GLContext glcontext = SDL_GL_CreateContext( window.get() );
     if( !glcontext ) {
         throw std::runtime_error("failed to create GL context");
     }
+    // ^ DO NOT DELETE ^
 
-    MPV::opengl_cb_init_gl( mpv_gl, [](void *, const char *name){
+    mpv_opengl_cb_init_gl( mpv_gl.get(), nullptr, [](void *, const char *name){
         return SDL_GL_GetProcAddress( name );
-    });
+    }, nullptr);
 
-    MPV::set_option_string( mpv, "vo", "opengl-cb" );
-
+    mpv_set_option_string( mpv.get(), "vo", "opengl-cb" );
 
     wakeup_on_mpv_events = SDL_RegisterEvents( 1 );
     mpv_set_wakeup_callback( mpv.get(), [](void*) {
         SDL_Event event ;
         event.type = wakeup_on_mpv_events;
         SDL_PushEvent( &event );
-    }, NULL );
+    }, nullptr );
 
     wakeup_on_mpv_redraw = SDL_RegisterEvents( 1 );
     mpv_opengl_cb_set_update_callback( mpv_gl.get(), [](void*) {
@@ -93,13 +88,56 @@ int main( int argc, char *argv[] )
         SDL_PushEvent( &event );
     }, NULL );
 
-
     // Play this file. Note that this starts playback asynchronously.
     const char *cmd[] = {"loadfile", mediaFile.c_str(), NULL};
     mpv_command( mpv.get(), cmd );
     double speed = 1.0;
     mpv_set_property( mpv.get(), "speed", MPV_FORMAT_DOUBLE, &speed );
     bool finished                 = false;
+
+
+    while (!finished) {
+        SDL_Event event;
+        if (SDL_WaitEvent(&event) != 1){
+            throw std::runtime_error("event loop error");
+        }
+        bool redraw = false;
+        switch (event.type) {
+        case SDL_QUIT:
+            finished = true ;
+        case SDL_WINDOWEVENT:
+            if (event.window.event == SDL_WINDOWEVENT_EXPOSED)
+                redraw = true;
+            break;
+        case SDL_KEYDOWN:
+            if (event.key.keysym.sym == SDLK_SPACE){
+                mpv_command_string(mpv.get(), "cycle pause");
+            }
+            break;
+        default:
+
+            if (event.type == wakeup_on_mpv_redraw){
+                redraw = true;
+            }
+            if (event.type == wakeup_on_mpv_events) {
+                while (1) {
+                    mpv_event *mp_event = mpv_wait_event(mpv.get(), 0);
+                    if (mp_event->event_id == MPV_EVENT_NONE){
+                        break;
+                    }
+                    std::cout << "event: " << mpv_event_name(mp_event->event_id) << std::endl ;
+                }
+            }
+        }
+        if (redraw) {
+            int w, h;
+            SDL_GetWindowSize(window.get(), &w, &h);
+            mpv_opengl_cb_draw(mpv_gl.get(), 0, w, -h);
+            SDL_GL_SwapWindow(window.get());
+        }
+    }
+
+    /*
     SDL::Event_Dispatcher handler = SDL::build( mpv,
                                                 wakeup_on_mpv_redraw,
                                                 wakeup_on_mpv_events,
@@ -120,6 +158,7 @@ int main( int argc, char *argv[] )
             default: break;
         }
     }
+    */
     SDL_Quit();
     return 0;
 }
---
+++ src/pointers.hpp
@@ -0,0 +1,34 @@
+#pragma once
+#include <memory>
+#include <SDL2/SDL.h>
+#include <mpv/client.h>
+#include <mpv/opengl_cb.h>
+#include <iostream>
+namespace std{
+    template <>
+    class default_delete <mpv_handle>{
+        public:
+            void operator()(mpv_handle* mpv) const {
+                mpv_terminate_destroy(mpv);
+                std::cout << "mpv_handle destroyed" << std::endl ;
+            } ;
+    };
+
+    template <>
+    class default_delete <mpv_opengl_cb_context>{
+        public:
+            void operator()(mpv_opengl_cb_context* mpv_gl) const {
+                mpv_opengl_cb_uninit_gl(mpv_gl);
+                std::cout << "mpv_opengl_cb_context destroyed" << std::endl ;
+            } ;
+    };
+
+    template <>
+    class default_delete <SDL_Window>{
+        public:
+            void operator()(SDL_Window* window) const {
+                SDL_DestroyWindow(window);
+                std::cout << "window destroyed" << std::endl ;
+            } ;
+    };
+}
